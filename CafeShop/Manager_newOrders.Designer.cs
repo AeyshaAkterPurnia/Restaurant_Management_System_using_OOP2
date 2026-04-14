b132315ef3c7c8ce816c85779b193d7f692bcb0b
Aeysha Akter Purnia <purniaislam7@gmail.com>
2026-04-14 08:20:44 +0600

    Add files via upload

---
+++ CafeShop/Manager_newOrders.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace cafeShopManagement
+{
+    partial class Manager_newOrders
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            panel_userDash = new Panel();
+            button_userDetails = new Button();
+            button_manager_orderHistory = new Button();
+            button_manager_add_user = new Button();
+            label_deshboard = new Label();
+            button_manager_checkFeedback = new Button();
+            button_manager_Orders = new Button();
+            button_manager_user = new Button();
+            button_profile = new Button();
+            label_manager = new Label();
+            button_Manager_Back = new Button();
+            label_title = new Label();
+            userDashboard_close = new Button();
+            label_newOrder = new Label();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            dataGridView_newOrder = new DataGridView();
+            panel_userDash.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView_newOrder).BeginInit();
+            SuspendLayout();
+            // 
+            // panel_userDash
+            // 
+            panel_userDash.BackColor = Color.FromArgb(111, 78, 55);
+            panel_userDash.Controls.Add(button_userDetails);
+            panel_userDash.Controls.Add(button_manager_orderHistory);
+            panel_userDash.Controls.Add(button_manager_add_user);
+            panel_userDash.Controls.Add(label_deshboard);
+            panel_userDash.Controls.Add(button_manager_checkFeedback);
+            panel_userDash.Controls.Add(button_manager_Orders);
+            panel_userDash.Controls.Add(button_manager_user);
+            panel_userDash.Controls.Add(button_profile);
+            panel_userDash.Controls.Add(label_manager);
+            panel_userDash.Location = new Point(-3, -3);
+            panel_userDash.Margin = new Padding(3, 4, 3, 4);
+            panel_userDash.Name = "panel_userDash";
+            panel_userDash.RightToLeft = RightToLeft.No;
+            panel_userDash.Size = new Size(192, 800);
+            panel_userDash.TabIndex = 12;
+            // 
+            // button_userDetails
+            // 
+            button_userDetails.BackColor = Color.FromArgb(111, 78, 55);
+            button_userDetails.BackgroundImageLayout = ImageLayout.None;
+            button_userDetails.FlatStyle = FlatStyle.Flat;
+            button_userDetails.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button_userDetails.ForeColor = Color.White;
+            button_userDetails.Location = new Point(15, 557);
+            button_userDetails.Margin = new Padding(3, 4, 3, 4);
+            button_userDetails.Name = "button_userDetails";
+            button_userDetails.Size = new Size(157, 40);
+            button_userDetails.TabIndex = 20;
+            button_userDetails.Text = "User Details";
+            button_userDetails.UseVisualStyleBackColor = false;
+            button_userDetails.Click += button_userDetails_Click;
+            // 
+            // button_manager_orderHistory
+            // 
+            button_manager_orderHistory.BackColor = Color.FromArgb(111, 78, 55);
+            button_manager_orderHistory.BackgroundImageLayout = ImageLayout.None;
+            button_manager_orderHistory.FlatStyle = FlatStyle.Flat;
+            button_manager_orderHistory.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button_manager_orderHistory.ForeColor = Color.White;
+            button_manager_orderHistory.Location = new Point(17, 436);
+            button_manager_orderHistory.Margin = new Padding(3, 4, 3, 4);
+            button_manager_orderHistory.Name = "button_manager_orderHistory";
+            button_manager_orderHistory.Size = new Size(157, 40);
+            button_manager_orderHistory.TabIndex = 18;
+            button_manager_orderHistory.Text = "Order History";
+            button_manager_orderHistory.UseVisualStyleBackColor = false;
+            button_manager_orderHistory.Click += button_manager_orderHistory_Click;
+            // 
+            // button_manager_add_user
+            // 
+            button_manager_add_user.BackColor = Color.FromArgb(111, 78, 55);
+            button_manager_add_user.BackgroundImageLayout = ImageLayout.None;
+            button_manager_add_user.FlatStyle = FlatStyle.Flat;
+            button_manager_add_user.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button_manager_add_user.ForeColor = Color.White;
+            button_manager_add_user.Location = new Point(15, 496);
+            button_manager_add_user.Margin = new Padding(3, 4, 3, 4);
+            button_manager_add_user.Name = "button_manager_add_user";
+            button_manager_add_user.Size = new Size(157, 40);
+            button_manager_add_user.TabIndex = 16;
+            button_manager_add_user.Text = "Add User";
+            button_manager_add_user.UseVisualStyleBackColor = false;
+            button_manager_add_user.Click += button_manager_add_user_Click;
+            // 
+            // label_deshboard
+            // 
+            label_deshboard.AutoSize = true;
+            label_deshboard.BackColor = Color.FromArgb(111, 78, 55);
+            label_deshboard.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label_deshboard.ForeColor = Color.White;
+            label_deshboard.Location = new Point(41, 81);
+            label_deshboard.Name = "label_deshboard";
+            label_deshboard.Size = new Size(119, 23);
+            label_deshboard.TabIndex = 15;
+            label_deshboard.Text = "Deshboard";
+            // 
+            // button_manager_checkFeedback
+            // 
+            button_manager_checkFeedback.BackColor = Color.FromArgb(111, 78, 55);
+            button_manager_checkFeedback.BackgroundImageLayout = ImageLayout.None;
+            button_manager_checkFeedback.FlatStyle = FlatStyle.Flat;
+            button_manager_checkFeedback.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button_manager_checkFeedback.ForeColor = Color.White;
+            button_manager_checkFeedback.Location = new Point(17, 619);
+            button_manager_checkFeedback.Margin = new Padding(3, 4, 3, 4);
+            button_manager_checkFeedback.Name = "button_manager_checkFeedback";
+            button_manager_checkFeedback.Size = new Size(157, 40);
+            button_manager_checkFeedback.TabIndex = 14;
+            button_manager_checkFeedback.Text = "Check Feedbacks";
+            button_manager_checkFeedback.UseVisualStyleBackColor = false;
+            button_manager_checkFeedback.Click += button_manager_checkFeedback_Click;
+            // 
+            // button_manager_Orders
+            // 
+            button_manager_Orders.BackColor = Color.FromArgb(111, 78, 55);
+            button_manager_Orders.BackgroundImageLayout = ImageLayout.None;
+            button_manager_Orders.FlatStyle = FlatStyle.Flat;
+            button_manager_Orders.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button_manager_Orders.ForeColor = Color.Black;
+            button_manager_Orders.Location = new Point(17, 375);
+            button_manager_Orders.Margin = new Padding(3, 4, 3, 4);
+            button_manager_Orders.Name = "button_manager_Orders";
+            button_manager_Orders.Size = new Size(157, 40);
+            button_manager_Orders.TabIndex = 14;
+            button_manager_Orders.Text = "New Order";
+            button_manager_Orders.UseVisualStyleBackColor = false;
+            // 
+            // button_manager_user
+            // 
+            button_manager_user.BackColor = Color.FromArgb(111, 78, 55);
+            button_manager_user.BackgroundImageLayout = ImageLayout.None;
+            button_manager_user.FlatStyle = FlatStyle.Flat;
+            button_manager_user.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button_manager_user.ForeColor = Color.White;
+            button_manager_user.Location = new Point(17, 309);
+            button_manager_user.Margin = new Padding(3, 4, 3, 4);
+            button_manager_user.Name = "button_manager_user";
+            button_manager_user.Size = new Size(157, 40);
+            button_manager_user.TabIndex = 14;
+            button_manager_user.Text = "Users List";
+            button_manager_user.UseVisualStyleBackColor = false;
+            button_manager_user.Click += button_manager_user_Click;
+            // 
+            // button_profile
+            // 
+            button_profile.BackColor = Color.FromArgb(111, 78, 55);
+            button_profile.BackgroundImageLayout = ImageLayout.None;
+            button_profile.FlatStyle = FlatStyle.Flat;
+            button_profile.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button_profile.ForeColor = Color.White;
+            button_profile.Location = new Point(17, 245);
+            button_profile.Margin = new Padding(3, 4, 3, 4);
+            button_profile.Name = "button_profile";
+            button_profile.Size = new Size(157, 40);
+            button_profile.TabIndex = 13;
+            button_profile.Text = "Profile";
+            button_profile.UseVisualStyleBackColor = false;
+            button_profile.Click += button_profile_Click;
+            // 
+            // label_manager
+            // 
+            label_manager.AutoSize = true;
+            label_manager.BackColor = Color.FromArgb(111, 78, 55);
+            label_manager.Font = new Font("Arial Rounded MT Bold", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label_manager.ForeColor = Color.White;
+            label_manager.Location = new Point(42, 36);
+            label_manager.Name = "label_manager";
+            label_manager.Size = new Size(120, 28);
+            label_manager.TabIndex = 12;
+            label_manager.Text = "Manager ";
+            // 
+            // button_Manager_Back
+            // 
+            button_Manager_Back.BackColor = Color.FromArgb(111, 78, 55);
+            button_Manager_Back.BackgroundImageLayout = ImageLayout.None;
+            button_Manager_Back.FlatStyle = FlatStyle.Flat;
+            button_Manager_Back.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button_Manager_Back.ForeColor = Color.White;
+            button_Manager_Back.Location = new Point(46, 747);
+            button_Manager_Back.Margin = new Padding(3, 4, 3, 4);
+            button_Manager_Back.Name = "button_Manager_Back";
+            button_Manager_Back.Size = new Size(109, 40);
+            button_Manager_Back.TabIndex = 19;
+            button_Manager_Back.Text = "Back";
+            button_Manager_Back.UseVisualStyleBackColor = false;
+            button_Manager_Back.Click += button_Manager_Back_Click;
+            // 
+            // label_title
+            // 
+            label_title.AutoSize = true;
+            label_title.Font = new Font("Arial Rounded MT Bold", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label_title.ForeColor = Color.FromArgb(111, 78, 55);
+            label_title.Location = new Point(398, 8);
+            label_title.Name = "label_title";
+            label_title.Size = new Size(480, 34);
+            label_title.TabIndex = 20;
+            label_title.Text = "Cafe Shop Management System";
+            // 
+            // userDashboard_close
+            // 
+            userDashboard_close.Font = new Font("Arial Rounded MT Bold", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            userDashboard_close.Location = new Point(997, -3);
+            userDashboard_close.Margin = new Padding(3, 4, 3, 4);
+            userDashboard_close.Name = "userDashboard_close";
+            userDashboard_close.Size = new Size(33, 35);
+            userDashboard_close.TabIndex = 21;
+            userDashboard_close.Text = "X";
+            userDashboard_close.UseVisualStyleBackColor = true;
+            userDashboard_close.Click += userDashboard_close_Click;
+            // 
+            // label_newOrder
+            // 
+            label_newOrder.AutoSize = true;
+            label_newOrder.BackColor = Color.White;
+            label_newOrder.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label_newOrder.ForeColor = Color.Black;
+            label_newOrder.Location = new Point(535, 140);
+            label_newOrder.Name = "label_newOrder";
+            label_newOrder.Size = new Size(119, 27);
+            label_newOrder.TabIndex = 19;
+            label_newOrder.Text = "New Order";
+            label_newOrder.Click += label_newOrder_Click;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new Size(20, 20);
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // dataGridView_newOrder
+            // 
+            dataGridView_newOrder.BackgroundColor = Color.White;
+            dataGridView_newOrder.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView_newOrder.GridColor = Color.White;
+            dataGridView_newOrder.Location = new Point(229, 197);
+            dataGridView_newOrder.Margin = new Padding(3, 4, 3, 4);
+            dataGridView_newOrder.Name = "dataGridView_newOrder";
+            dataGridView_newOrder.RowHeadersWidth = 51;
+            dataGridView_newOrder.Size = new Size(734, 420);
+            dataGridView_newOrder.TabIndex = 22;
+            // 
+            // Manager_newOrders
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1029, 800);
+            Controls.Add(dataGridView_newOrder);
+            Controls.Add(label_newOrder);
+            Controls.Add(userDashboard_close);
+            Controls.Add(label_title);
+            Controls.Add(button_Manager_Back);
+            Controls.Add(panel_userDash);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Manager_newOrders";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Manager_newOrders";
+            Load += Manager_newOrders_Load;
+            panel_userDash.ResumeLayout(false);
+            panel_userDash.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView_newOrder).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel_userDash;
+        private Button button_manager_orderHistory;
+        private Button button_manager_add_user;
+        private Label label_deshboard;
+        private Button button_manager_checkFeedback;
+        private Button button_manager_Orders;
+        private Button button_manager_user;
+        private Button button_profile;
+        private Label label_manager;
+        private Button button_Manager_Back;
+        private Label label_title;
+        private Button userDashboard_close;
+        private Label label_newOrder;
+        private ContextMenuStrip contextMenuStrip1;
+        private DataGridView dataGridView_newOrder;
+        private Button button_userDetails;
+    }
+}
---
+++ CafeShop/Manager_newOrders.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cafeShopManagement
+{
+    public partial class Manager_newOrders : Form
+    {
+        public Manager_newOrders()
+        {
+            InitializeComponent();
+        }
+
+        private void button_profile_Click(object sender, EventArgs e)
+        {
+            ManagerProfile m1 = new ManagerProfile();
+            m1.Show();
+            this.Hide();
+        }
+
+        private void button_manager_user_Click(object sender, EventArgs e)
+        {
+            Manager_userList manager_UserList = new Manager_userList();
+            manager_UserList.Show();
+            this.Hide();
+        }
+
+        private void userDashboard_close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button_Manager_Back_Click(object sender, EventArgs e)
+        {
+            ManagerDeshboard m1 = new ManagerDeshboard();
+            m1.Show();
+            this.Hide();
+        }
+
+        private void label_newOrder_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_manager_orderHistory_Click(object sender, EventArgs e)
+        {
+            Manager_orderHistory m1 = new Manager_orderHistory();
+            m1.Show();
+            this.Hide();
+        }
+
+        private void button_manager_add_user_Click(object sender, EventArgs e)
+        {
+            Manager_AddUser addUser = new Manager_AddUser();
+            addUser.Show();
+            this.Hide();
+
+        }
+
+        private void button_manager_add_deliveryMan_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_manager_checkFeedback_Click(object sender, EventArgs e)
+        {
+            Manager_checkFeedbacks manager_CheckFeedbacks = new Manager_checkFeedbacks();
+            manager_CheckFeedbacks.Show();
+            this.Hide();
+        }
+
+        private void button_userDetails_Click(object sender, EventArgs e)
+        {
+            Manager_userDetails m1 = new Manager_userDetails();
+            m1.Show();
+            this.Hide();
+        }
+
+        private void Manager_newOrders_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
