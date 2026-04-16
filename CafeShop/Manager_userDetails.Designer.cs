ba25df741fcf062fde5e58d18cf3993fd3db5416
Aeysha Akter Purnia <purniaislam7@gmail.com>
2026-04-16 11:58:00 +0600

    Add files via upload

---
+++ CafeShop/Manager_userDetails.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace cafeShopManagement
+{
+    partial class Manager_userDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            userDashboard_close = new Button();
+            panel_userDash = new Panel();
+            button_Manager_Back = new Button();
+            button_userDetails = new Button();
+            button_manager_orderHistory = new Button();
+            button_manager_add_user = new Button();
+            label_deshboard = new Label();
+            button_manager_checkFeedback = new Button();
+            button_manager_Orders = new Button();
+            button_manager_user = new Button();
+            button_profile = new Button();
+            label_manager = new Label();
+            label_title = new Label();
+            dataGridView_userDetails = new DataGridView();
+            label_userDetails = new Label();
+            button_search = new Button();
+            textBox_search = new TextBox();
+            label_manager_search_userName = new Label();
+            panel_userDash.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView_userDetails).BeginInit();
+            SuspendLayout();
+            // 
+            // userDashboard_close
+            // 
+            userDashboard_close.Font = new Font("Arial Rounded MT Bold", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            userDashboard_close.Location = new Point(872, -2);
+            userDashboard_close.Name = "userDashboard_close";
+            userDashboard_close.Size = new Size(29, 26);
+            userDashboard_close.TabIndex = 22;
+            userDashboard_close.Text = "X";
+            userDashboard_close.UseVisualStyleBackColor = true;
+            userDashboard_close.Click += userDashboard_close_Click;
+            // 
+            // panel_userDash
+            // 
+            panel_userDash.BackColor = Color.FromArgb(111, 78, 55);
+            panel_userDash.Controls.Add(button_Manager_Back);
+            panel_userDash.Controls.Add(button_userDetails);
+            panel_userDash.Controls.Add(button_manager_orderHistory);
+            panel_userDash.Controls.Add(button_manager_add_user);
+            panel_userDash.Controls.Add(label_deshboard);
+            panel_userDash.Controls.Add(button_manager_checkFeedback);
+            panel_userDash.Controls.Add(button_manager_Orders);
+            panel_userDash.Controls.Add(button_manager_user);
+            panel_userDash.Controls.Add(button_profile);
+            panel_userDash.Controls.Add(label_manager);
+            panel_userDash.Location = new Point(-3, -2);
+            panel_userDash.Name = "panel_userDash";
+            panel_userDash.RightToLeft = RightToLeft.No;
+            panel_userDash.Size = new Size(168, 600);
+            panel_userDash.TabIndex = 23;
+            // 
+            // button_Manager_Back
+            // 
+            button_Manager_Back.BackColor = Color.FromArgb(111, 78, 55);
+            button_Manager_Back.BackgroundImageLayout = ImageLayout.None;
+            button_Manager_Back.FlatStyle = FlatStyle.Flat;
+            button_Manager_Back.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button_Manager_Back.ForeColor = Color.White;
+            button_Manager_Back.Location = new Point(37, 560);
+            button_Manager_Back.Name = "button_Manager_Back";
+            button_Manager_Back.Size = new Size(95, 30);
+            button_Manager_Back.TabIndex = 24;
+            button_Manager_Back.Text = "Back";
+            button_Manager_Back.UseVisualStyleBackColor = false;
+            button_Manager_Back.Click += button_Manager_Back_Click_1;
+            // 
+            // button_userDetails
+            // 
+            button_userDetails.BackColor = Color.FromArgb(111, 78, 55);
+            button_userDetails.BackgroundImageLayout = ImageLayout.None;
+            button_userDetails.FlatStyle = FlatStyle.Flat;
+            button_userDetails.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button_userDetails.ForeColor = Color.Black;
+            button_userDetails.Location = new Point(13, 417);
+            button_userDetails.Name = "button_userDetails";
+            button_userDetails.Size = new Size(137, 30);
+            button_userDetails.TabIndex = 20;
+            button_userDetails.Text = "User Details";
+            button_userDetails.UseVisualStyleBackColor = false;
+            button_userDetails.Click += button_userDetails_Click;
+            // 
+            // button_manager_orderHistory
+            // 
+            button_manager_orderHistory.BackColor = Color.FromArgb(111, 78, 55);
+            button_manager_orderHistory.BackgroundImageLayout = ImageLayout.None;
+            button_manager_orderHistory.FlatStyle = FlatStyle.Flat;
+            button_manager_orderHistory.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button_manager_orderHistory.ForeColor = Color.White;
+            button_manager_orderHistory.Location = new Point(15, 327);
+            button_manager_orderHistory.Name = "button_manager_orderHistory";
+            button_manager_orderHistory.Size = new Size(137, 30);
+            button_manager_orderHistory.TabIndex = 18;
+            button_manager_orderHistory.Text = "Order History";
+            button_manager_orderHistory.UseVisualStyleBackColor = false;
+            button_manager_orderHistory.Click += button_manager_orderHistory_Click_1;
+            // 
+            // button_manager_add_user
+            // 
+            button_manager_add_user.BackColor = Color.FromArgb(111, 78, 55);
+            button_manager_add_user.BackgroundImageLayout = ImageLayout.None;
+            button_manager_add_user.FlatStyle = FlatStyle.Flat;
+            button_manager_add_user.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button_manager_add_user.ForeColor = Color.White;
+            button_manager_add_user.Location = new Point(13, 372);
+            button_manager_add_user.Name = "button_manager_add_user";
+            button_manager_add_user.Size = new Size(137, 30);
+            button_manager_add_user.TabIndex = 16;
+            button_manager_add_user.Text = "Add User";
+            button_manager_add_user.UseVisualStyleBackColor = false;
+            button_manager_add_user.Click += button_manager_add_user_Click_1;
+            // 
+            // label_deshboard
+            // 
+            label_deshboard.AutoSize = true;
+            label_deshboard.BackColor = Color.FromArgb(111, 78, 55);
+            label_deshboard.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label_deshboard.ForeColor = Color.White;
+            label_deshboard.Location = new Point(36, 61);
+            label_deshboard.Name = "label_deshboard";
+            label_deshboard.Size = new Size(96, 18);
+            label_deshboard.TabIndex = 15;
+            label_deshboard.Text = "Deshboard";
+            // 
+            // button_manager_checkFeedback
+            // 
+            button_manager_checkFeedback.BackColor = Color.FromArgb(111, 78, 55);
+            button_manager_checkFeedback.BackgroundImageLayout = ImageLayout.None;
+            button_manager_checkFeedback.FlatStyle = FlatStyle.Flat;
+            button_manager_checkFeedback.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button_manager_checkFeedback.ForeColor = Color.White;
+            button_manager_checkFeedback.Location = new Point(15, 464);
+            button_manager_checkFeedback.Name = "button_manager_checkFeedback";
+            button_manager_checkFeedback.Size = new Size(137, 30);
+            button_manager_checkFeedback.TabIndex = 14;
+            button_manager_checkFeedback.Text = "Check Feedbacks";
+            button_manager_checkFeedback.UseVisualStyleBackColor = false;
+            button_manager_checkFeedback.Click += button_manager_checkFeedback_Click_1;
+            // 
+            // button_manager_Orders
+            // 
+            button_manager_Orders.BackColor = Color.FromArgb(111, 78, 55);
+            button_manager_Orders.BackgroundImageLayout = ImageLayout.None;
+            button_manager_Orders.FlatStyle = FlatStyle.Flat;
+            button_manager_Orders.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button_manager_Orders.ForeColor = Color.White;
+            button_manager_Orders.Location = new Point(15, 281);
+            button_manager_Orders.Name = "button_manager_Orders";
+            button_manager_Orders.Size = new Size(137, 30);
+            button_manager_Orders.TabIndex = 14;
+            button_manager_Orders.Text = "New Order";
+            button_manager_Orders.UseVisualStyleBackColor = false;
+            button_manager_Orders.Click += button_manager_Orders_Click_1;
+            // 
+            // button_manager_user
+            // 
+            button_manager_user.BackColor = Color.FromArgb(111, 78, 55);
+            button_manager_user.BackgroundImageLayout = ImageLayout.None;
+            button_manager_user.FlatStyle = FlatStyle.Flat;
+            button_manager_user.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button_manager_user.ForeColor = Color.White;
+            button_manager_user.Location = new Point(15, 232);
+            button_manager_user.Name = "button_manager_user";
+            button_manager_user.Size = new Size(137, 30);
+            button_manager_user.TabIndex = 14;
+            button_manager_user.Text = "Users List";
+            button_manager_user.UseVisualStyleBackColor = false;
+            button_manager_user.Click += button_manager_user_Click_1;
+            // 
+            // button_profile
+            // 
+            button_profile.BackColor = Color.FromArgb(111, 78, 55);
+            button_profile.BackgroundImageLayout = ImageLayout.None;
+            button_profile.FlatStyle = FlatStyle.Flat;
+            button_profile.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button_profile.ForeColor = Color.White;
+            button_profile.Location = new Point(15, 184);
+            button_profile.Name = "button_profile";
+            button_profile.Size = new Size(137, 30);
+            button_profile.TabIndex = 13;
+            button_profile.Text = "Profile";
+            button_profile.UseVisualStyleBackColor = false;
+            button_profile.Click += button_profile_Click_1;
+            // 
+            // label_manager
+            // 
+            label_manager.AutoSize = true;
+            label_manager.BackColor = Color.FromArgb(111, 78, 55);
+            label_manager.Font = new Font("Arial Rounded MT Bold", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label_manager.ForeColor = Color.White;
+            label_manager.Location = new Point(37, 27);
+            label_manager.Name = "label_manager";
+            label_manager.Size = new Size(95, 22);
+            label_manager.TabIndex = 12;
+            label_manager.Text = "Manager ";
+            // 
+            // label_title
+            // 
+            label_title.AutoSize = true;
+            label_title.Font = new Font("Arial Rounded MT Bold", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label_title.ForeColor = Color.FromArgb(111, 78, 55);
+            label_title.Location = new Point(348, 6);
+            label_title.Name = "label_title";
+            label_title.Size = new Size(376, 28);
+            label_title.TabIndex = 24;
+            label_title.Text = "Cafe Shop Management System";
+            // 
+            // dataGridView_userDetails
+            // 
+            dataGridView_userDetails.BackgroundColor = Color.White;
+            dataGridView_userDetails.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView_userDetails.GridColor = Color.White;
+            dataGridView_userDetails.Location = new Point(218, 182);
+            dataGridView_userDetails.Name = "dataGridView_userDetails";
+            dataGridView_userDetails.Size = new Size(642, 315);
+            dataGridView_userDetails.TabIndex = 26;
+            // 
+            // label_userDetails
+            // 
+            label_userDetails.AutoSize = true;
+            label_userDetails.BackColor = Color.White;
+            label_userDetails.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label_userDetails.ForeColor = Color.Black;
+            label_userDetails.Location = new Point(472, 139);
+            label_userDetails.Name = "label_userDetails";
+            label_userDetails.Size = new Size(100, 21);
+            label_userDetails.TabIndex = 25;
+            label_userDetails.Text = "User Details";
+            // 
+            // button_search
+            // 
+            button_search.BackColor = Color.FromArgb(111, 78, 55);
+            button_search.BackgroundImageLayout = ImageLayout.None;
+            button_search.Cursor = Cursors.Cross;
+            button_search.FlatStyle = FlatStyle.Flat;
+            button_search.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button_search.ForeColor = Color.White;
+            button_search.Location = new Point(185, 116);
+            button_search.Name = "button_search";
+            button_search.Size = new Size(137, 30);
+            button_search.TabIndex = 27;
+            button_search.Text = "Search";
+            button_search.UseVisualStyleBackColor = false;
+            // 
+            // textBox_search
+            // 
+            textBox_search.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox_search.Location = new Point(185, 84);
+            textBox_search.Name = "textBox_search";
+            textBox_search.Size = new Size(213, 26);
+            textBox_search.TabIndex = 29;
+            // 
+            // label_manager_search_userName
+            // 
+            label_manager_search_userName.AutoSize = true;
+            label_manager_search_userName.BackColor = Color.White;
+            label_manager_search_userName.Font = new Font("Arial Rounded MT Bold", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label_manager_search_userName.ForeColor = Color.Black;
+            label_manager_search_userName.Location = new Point(185, 59);
+            label_manager_search_userName.Name = "label_manager_search_userName";
+            label_manager_search_userName.Size = new Size(106, 22);
+            label_manager_search_userName.TabIndex = 28;
+            label_manager_search_userName.Text = "UserName";
+            // 
+            // Manager_userDetails
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(900, 600);
+            Controls.Add(button_search);
+            Controls.Add(textBox_search);
+            Controls.Add(label_manager_search_userName);
+            Controls.Add(dataGridView_userDetails);
+            Controls.Add(label_userDetails);
+            Controls.Add(label_title);
+            Controls.Add(panel_userDash);
+            Controls.Add(userDashboard_close);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Manager_userDetails";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Manager_userDetails";
+            Load += Manager_userDetails_Load;
+            panel_userDash.ResumeLayout(false);
+            panel_userDash.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView_userDetails).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button userDashboard_close;
+        private Panel panel_userDash;
+        private Button button_userDetails;
+        private Button button_manager_orderHistory;
+        private Button button_manager_add_user;
+        private Label label_deshboard;
+        private Button button_manager_checkFeedback;
+        private Button button_manager_Orders;
+        private Button button_manager_user;
+        private Button button_profile;
+        private Label label_manager;
+        private Button button_Manager_Back;
+        private Label label_title;
+        private DataGridView dataGridView_userDetails;
+        private Label label_userDetails;
+        private Button button_search;
+        private TextBox textBox_search;
+        private Label label_manager_search_userName;
+    }
+}
